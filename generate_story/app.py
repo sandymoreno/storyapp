85b57463e1859d032e6b80d8696a101ddd03dda3
Sandy Moreno <it@holiplus.com>
2023-04-06 22:49:13 -0500

    adjust template

--- generate_story/app.py
+++ generate_story/app.py
@@ -1,42 +1,85 @@
+import os
 import json
+import uuid
+import boto3
+import requests
+import datetime
 
-# import requests
+# Retrieve the secret from AWS Parameter Store
+
+
+def get_secret(ssm_param_name):
+    session = boto3.session.Session()
+    ssm = session.client('ssm')
+
+    # Retrieve the secret
+    response = ssm.get_parameter(Name=ssm_param_name, WithDecryption=True)
+    secret_value = response['Parameter']['Value']
+    return secret_value
+
+# AWS Lambda handler function
+
+
+def save_story(story):
+
+    # Save the story to DynamoDB
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('stories')
+    table.put_item(Item=story)
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    # Retrieve the OpenAI API key from AWS Parameter Store
+    openai_api_key = get_secret("/storysam/openai-key")
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    prompt = '''
+    Create a story suitable for young children. Consider stories with simple sentence structures and familiar vocabulary for young readers. Use relatable characters and situations that children of this age can understand and empathize with.
+    Include elements of adventure, imagination, and humor to keep the stories engaging and entertaining.Keep in mind that the stories should also have a positive message or teach a valuable lesson for young readers. Consider using animals or other non-human characters as they can be relatable to young readers and allow for more imaginative elements to be incorporated. Use dialogue to give the characters personalities and make them more relatable to young readers. 
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    Follow the following istructions:
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    - Use Markdown language with headers and proper format.
+    - Finish the moral from the story.
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    Topic: A young prince or princess who learns about responsibility and leadership as they rule their kingdom.
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
+    At the end, write a prompt to generate an image using DALLEE withe the following structure:
 
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
+    “Imagine a children book illustration with this idea: {principal idea of the story}”
+    
+    '''
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
+    # Call the OpenAI GPT API
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + openai_api_key
+    }
+    data = {
+        'model': 'text-davinci-003',
+        'prompt': prompt,
+        'temperature': 0.7,
+        'max_tokens': 1000,
+        'top_p': 1,
+        'frequency_penalty': 0,
+        'presence_penalty': 0
+    }
+    response = requests.post(
+        'https://api.openai.com/v1/completions', headers=headers, json=data)
+    response_json = response.json()
 
-    #     raise e
+    # Return the chatbot's response
+    # chatbot_response = response_json['choices'][0]['text']
+    chatbot_response = '## Story'
+
+    # Save the story to DynamoDB
+    story = {
+        'id': uuid.uuid4().hex,
+        'story': chatbot_response,
+        'timestamp': datetime.datetime.now().isoformat()
+    }
+    save_story(story)
 
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'statusCode': 200,
+        'body': chatbot_response
     }
